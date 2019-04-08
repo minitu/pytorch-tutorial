ff0d4f3f4c0f7cca72f619dff31ae12cf418efea
Jaemin Choi <jchoi157@illinois.edu>
2019-04-08 17:16:25 -0400

    Add epoch timer and reproducibility option

--- tutorials/03-advanced/image_captioning/train.py
+++ tutorials/03-advanced/image_captioning/train.py
@@ -27,6 +27,20 @@ class Worker:
     self.n_cpu_workers = (self.local_size - self.n_gpus) * self.n_nodes
     self.n_gpu_workers = self.n_gpus * self.n_nodes
 
+    # Set RNG seed for reproducibility, can be left on
+    torch.manual_seed(1234)
+
+    # CuDNN reproducibility
+    if args.reproducible:
+      torch.backends.cudnn.deterministic = True
+      torch.backends.cudnn.benchmark = False
+
+    # Set number of threads
+    if self.dist.is_cpu_rank():
+      #torch.set_num_threads(args.num_threads)
+      print("[Rank {}] Setting number of OMP threads to {}".format(self.rank,
+        args.num_threads), flush=True)
+
     # Calculate batch sizes
     self.total_batch_size = args.batch_size
     self.cpu_batch_size = args.cpu_batch_size
@@ -92,7 +106,8 @@ class Worker:
     data_loader = get_loader(args.image_dir, args.caption_path, vocab,
                              transform, self.rank, self.world_size, self.local_size,
                              self.n_gpus, self.total_batch_size, self.cpu_batch_size,
-                             self.gpu_batch_size, self.batch_size, shuffle=True,
+                             self.gpu_batch_size, self.batch_size,
+                             shuffle=(False if args.reproducible else True),
                              no_partition = args.no_partition)
     self.num_batches = len(data_loader)
     print("[Rank {}] batch size {}, num batches {}".format(self.rank,
@@ -113,6 +128,7 @@ class Worker:
 
     # Train the models
     total_step = len(data_loader)
+    epoch_start_time = time.time()
     for epoch in range(args.num_epochs):
       processed_batches = 0
       batch_time_sum = 0
@@ -139,10 +155,11 @@ class Worker:
         batch_time_sum += batch_time
         processed_batches += 1
 
+        saved_loss = loss.item()
         # Print log info
         if i % args.log_step == 0 and i != 0:
           print('Rank [{}], Epoch [{}/{}], Step [{}/{}], Average time: {:.6f}, Loss: {:.4f}, Perplexity: {:5.4f}'
-                  .format(self.rank, epoch, args.num_epochs, i, total_step, batch_time_sum / processed_batches, loss.item(), np.exp(loss.item())), flush=True)
+                  .format(self.rank, epoch, args.num_epochs, i, total_step, batch_time_sum / processed_batches, saved_loss, np.exp(saved_loss)), flush=True)
           batch_time_sum = 0
           processed_batches = 0
 
@@ -154,6 +171,9 @@ class Worker:
             args.model_path, 'encoder-{}-{}.ckpt'.format(epoch+1, i+1)))
 
         batch_start_time = time.time()
+
+      epoch_time = time.time() - epoch_start_time
+      print('!!! Rank [{}], Epoch [{}], Time: {:.6f}, Loss: {:.4f}, Perplexity: {:5.4f}'.format(self.rank, epoch, epoch_time, saved_loss, np.exp(saved_loss)), flush=True)
 
 def main():
   # Training settings
@@ -174,15 +194,14 @@ def main():
   parser.add_argument('-e', '--num-epochs', type=int, default=5)
   parser.add_argument('-b', '--batch-size', type=int, default=128)
   parser.add_argument('-c', '--cpu-batch-size', type=int, default=24)
+  parser.add_argument('-t', '--num-threads', type=int, default=30)
   parser.add_argument('-l', '--learning-rate', type=float, default=0.001)
   parser.add_argument('-g', '--gpu-only', action='store_true', default=False, help="use only GPU workers, number of ranks should be the same as the number of GPUs")
   parser.add_argument('--no-partition', action='store_true', default=False, help="do not partition the batch across workers, used for performance comparison")
+  parser.add_argument('-r', '--reproducible', action='store_true', default=False, help="make runs reproducible")
 
   args = parser.parse_args()
   print(args, flush=True)
-
-  # Set RNG seed so that calls to rand() are reproducible
-  torch.manual_seed(1234)
 
   # Create worker abstraction
   worker = Worker(args)
